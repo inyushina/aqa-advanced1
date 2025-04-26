b7e04d3af268352e4d45b38f9739434006efa5d1
Kristina In <chris.inyushina@gmail.com>
2025-04-27 00:21:39 +0300

    branch Lesson 3 was created with a new folder nodeBasics and file index.js

---
+++ nodeBasics/index.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+console.log(chalk.blue('Thanks God its Weekend!'))
+console.log(chalk.yellowBright('Woooooooooooow!!!!!!'));
+console.log(chalk.bgRedBright('Today') + chalk.greenBright('is') + chalk.bgBlue('Saturday!'))
