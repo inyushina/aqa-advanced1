f2c3e204c070c4a9151c8b4b0ce38337e878df40
Kristina In <chris.inyushina@gmail.com>
2025-05-04 09:38:04 +0300

    homework for lesson5

---
+++ lesson 5 HW/task3(5).js
@@ -0,0 +1,16 @@
+// for(let i = 1; i <= 10; i++)
+//     console.log(i*5)
+
+//for
+const number = 5;
+for (let i = 1; i <= 10; i++){
+    console.log(`${number}*${i}=${number*i}`)
+}
+
+
+//while
+const number1 = 5;
+let i = 1;
+while (i <=10){console.log(`${number1}*${i}=${number1*i}`)
+i++};
+    
