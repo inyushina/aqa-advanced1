aa0270a7e42fe5279195608b1abd63e27e581158
Kristina In <chris.inyushina@gmail.com>
2025-05-19 22:12:44 +0300

    homework for lesson 8

---
+++ lesson8/task3(8).js
@@ -0,0 +1,3 @@
+const massive = [10, 20, 30, 40, 50];
+const sum = massive.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(sum);
---
+++ lesson8/task6(8).js
@@ -0,0 +1,5 @@
+const numbersList = [1,10,14,2,4,5,43,34];
+const copycatList = [1,10,14,2,4,5,43,34];
+//copycatList.reverse();
+copycatList.sort((a,b) => a-b)
+console.log(numbersList ,copycatList)
